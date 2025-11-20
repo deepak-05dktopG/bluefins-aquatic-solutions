fc9b9b34d8b2bd42453f18a836195f344eb4702a
Your Name <your.email@example.com>
2025-11-20 14:40:57 +0530

    Fix case-sensitive paths: use AdminPanel instead of adminPanel

--- client/src/pages/AdminPanel/index.js
+++ client/src/pages/AdminPanel/index.js
@@ -1,6 +1,6 @@
-export { default as AdminLogin } from './AdminLogin';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as LessonPlans } from './LessonPlans';
-export { default as MembersFeedback } from './MembersFeedback';
-export { default as WeeklyWorksheets } from './WeeklyWorksheets';
-export { default as Posts } from './Posts';
+export { default as AdminLogin } from './AdminLogin.jsx';
+export { default as AdminDashboard } from './AdminDashboard.jsx';
+export { default as LessonPlans } from './LessonPlans.jsx';
+export { default as MembersFeedback } from './MembersFeedback.jsx';
+export { default as WeeklyWorksheets } from './WeeklyWorksheets.jsx';
+export { default as Posts } from './Posts.jsx';
